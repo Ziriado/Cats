ffe019df829d4a304f3829ab7c0cf04c6cb5ce43
Ziriado <115992429+Ziriado@users.noreply.github.com>
2023-04-13 10:09:50 +0200

    some updates

--- script/startpagescript.js
+++ script/startpagescript.js
@@ -1,12 +1,11 @@
 var matrix = Array(5).fill(null).map(() => Array(5).fill(0));
-matrix[0][1] = 90;
-matrix[1][3] = 70;
 let row = 5;
 let collum = 5;
-let x = 5;
-let y = 0;
-let drawcount=0;
-
+let x = 0;
+let y = 4;
+let drawcount = 0;
+let life = 1;
+let savedCats = 0;
 const arr = ["O", "Zombie", "Cat", "O", "Cat", "O", "O"];
 
 drawTable();
@@ -14,27 +13,56 @@ getButtonsWest();
 getButtonsEast();
 getButtonsNorth();
 getButtonsSouth();
+function collisionCats()
+{
+    if(matrix[y][x]=='Cat')
+    {
+        savedCats++;
+    }
+}
+function collisionZombie()
+{
+    if(matrix[y][x]=='Zombie')
+    {
+        life--;
+    }
+}
 function drawTable() {
     let table = "";
     table += "<table>"
     let arrayLength = arr.length;
-    if(drawcount==0){
+    collisionCats();
+    collisionZombie();
 
-        for (let i = 0; i < row; i++) {
-            for (let j = 0; j < collum; j++) {
-                const rndInt = arrayRandom(0, arrayLength - 1)
-                table += "<td>" + arr[rndInt] + "</td>"
-                
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < collum; j++) {
+
+            const rndInt = arrayRandom(0, arrayLength - 1)
+            let getObject = arr[rndInt]
+            if (i == 4 && j == 0 && drawcount == 0) {
+                getObject = 'X'
             }
-            table += "</tr>"
+            if (drawcount == 0) {
+                table += "<td>" + getObject + "</td>"
+                matrix[i][j] = getObject
+            }
+            if (drawcount >= 1) {
+                let drawout = matrix[i][j]
+                matrix[y][x] = 'X'
+                table += "<td>" + drawout + "</td>"
+            }
         }
-        table += "</table>"
-        document.getElementById("table").innerHTML = table
-        drawcount++;
+        table += "</tr>"
     }
-    //console.table(matrix)
+    table += "</table>"
+    document.getElementById("table").innerHTML = table
+    drawcount++;
+
+    console.table(matrix)
     console.log("Value of x: " + x)
     console.log("Value of y: " + y)
+    console.log("Saved cats: "+savedCats)
+    console.log("Life remaning: "+life)
 }
 
 function arrayRandom(min, max) {
@@ -42,12 +70,10 @@ function arrayRandom(min, max) {
 }
 
 function West() {
-    if(x ==0)
-    {
+    if (x == 0) {
         x
     }
-    else
-    {
+    else {
 
         x--
     }
@@ -55,12 +81,10 @@ function West() {
     drawTable()
 }
 function East() {
-    if(x == 4)
-    {
+    if (x == 4) {
         x
     }
-    else
-    {
+    else {
 
         x++
     }
@@ -68,12 +92,10 @@ function East() {
     drawTable()
 }
 function North() {
-    if(y==4)
-    {
+    if (y == 4) {
         y
     }
-    else
-    {
+    else {
         y++
 
     }
@@ -113,7 +135,7 @@ function getButtonsNorth() {
     post.innerHTML = ""
 
     let buttonNorth = document.createElement("button")
-    buttonNorth.innerHTML = "<button onclick='North()'>North</button> "
+    buttonNorth.innerHTML = "<button onclick='North()'>South</button> "
     post.appendChild(buttonNorth)
 }
 function getButtonsSouth() {
@@ -121,7 +143,7 @@ function getButtonsSouth() {
     post.innerHTML = ""
 
     let buttonSouth = document.createElement("button")
-    buttonSouth.innerHTML = "<button onclick='South()'>South</button> "
+    buttonSouth.innerHTML = "<button onclick='South()'>North</button> "
     post.appendChild(buttonSouth)
 }
 const rndInt = arrayRandom(1, 5)
